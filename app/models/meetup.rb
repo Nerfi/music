8b1e430c1d712d649ebcd9677543961179542fd6
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-12 20:31:49 +0200

    not working at all

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,8 +2,22 @@ class Meetup
   include HTTParty
   base_uri 'api.meetup.com'
 
+  attr_reader :options
+
+  def initialize
+    api_key  = ENV{MEET_UP_API_KEY}
+    @options = {
+      query: {
+        key: api_key,
+        sign: "true",
+        desc: "true",
+        page: 20
+      }
+    }
+  end
+
   def get_data
-    self.class.get('/operation-code-hampton-roads/events')
+    self.class.get('/operation-code-hampton-roads/events', @options)
   end
 
   def events
