32dee9cecb4057995d0d1a0b7612efc8c86caf68
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-12 18:16:51 +0200

    new API, meetup

--- test/controllers/songs_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SongsControllerTest < ActionDispatch::IntegrationTest
-  test "should get search" do
-    get songs_search_url
-    assert_response :success
-  end
-
-end
